fb43f445bf4654a066344e540ee956fdd990c19a
Sean-OBrien95 <>
2023-08-31 17:22:19 +0000

    Create buyer or seller function for user to select

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def instructions():
     """
     print("Welcome to Pre-Loved Pieces, the second hand buy and sell system!\n")
     while True:
-        answer = input("Do you need instructions? (y/n): ").lower()
+        answer = input("Do you need instructions? (y/n): \n").lower()
         if answer == "y":
             print("The system is used for buying a selling clothes items. You will select if you are buying or selling (b/s) and then get further instructions")
             break
@@ -27,5 +27,26 @@ def instructions():
             break
         else:
             print("Invalid input, choose y/n")
+    
 
+def buyer_or_seller():
+    """
+    Function to ask if user is buying or selling, and then return data to system
+    """
+    while True:
+        b_or_s = input("Are you a buyer or a seller? (b/s): \n")
+        if b_or_s == "b":
+            print("Loading list of items...")
+            break
+        elif b_or_s == "s":
+            print("Seller confirmed...")
+            break
+        else:
+            print("Invalid input, choose b/s")
+
+    return b_or_s
+    
 instructions()
+b_or_s = buyer_or_seller()
+print(b_or_s)
+
